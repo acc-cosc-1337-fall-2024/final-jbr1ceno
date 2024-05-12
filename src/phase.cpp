649ea35aff931aa25dd8edd396f15bbdb6ffc0fa
jbr1ceno <143046486+jbr1ceno@users.noreply.github.com>
2024-05-12 05:56:34 +0000

    Question 4 wiP

--- src/phase.cpp
+++ src/phase.cpp
@@ -1,1 +1,17 @@
 //
+#include<iostream>
+#include<vector>
+#include "roll.h"
+#include "die.h"
+
+#ifndef MY_PHASE_H
+#define MY_PHASE_H
+
+class Phase //an abstract class has one pure virtual function
+{
+public:
+    enum RollOutcome get_outcome(Roll* roll);
+private:
+};
+
+#endif
